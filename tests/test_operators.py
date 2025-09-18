d19cdf5ecada9415087931613f2fa26a5914832a
AnnaKarpovi <anna.karpoffa@gmail.com>
2025-09-18 14:59:08 +0300

    tests operators

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -107,41 +107,45 @@ def test_sigmoid(a: float) -> None:
     * It crosses 0 at 0.5
     * It is  strictly increasing.
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    s = sigmoid(a)
+    assert 0.0 <= s <= 1.0
+    assert_close(1.0 - s, sigmoid(-a))
+    assert_close(sigmoid(0.0), 0.5)
+    assert sigmoid(a + 1e-4) > s
+    assert sigmoid(a - 1e-4) < s
+
 
 
 @pytest.mark.task0_2
 @given(small_floats, small_floats, small_floats)
 def test_transitive(a: float, b: float, c: float) -> None:
     """Test the transitive property of less-than (a < b and b < c implies a < c)"""
-    # TODO: Implement for Task 0.2.
+    if lt(a, b) and lt(b, c): assert lt(a, c)
     raise NotImplementedError("Need to implement for Task 0.2")
 
 
 @pytest.mark.task0_2
-def test_symmetric() -> None:
+def test_symmetric(x: float, y: float) -> None:
     """Write a test that ensures that :func:`minitorch.operators.mul` is symmetric, i.e.
     gives the same value regardless of the order of its input.
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert_close(mul(x, y), mul(y, x))
 
 
 @pytest.mark.task0_2
-def test_distribute() -> None:
+def test_distribute(x: float, y: float, z:float ) -> None:
     r"""Write a test that ensures that your operators distribute, i.e.
     :math:`z \times (x + y) = z \times x + z \times y`
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert_close(mul(z, add(x, y)), add(mul(z, x), mul(z, y)))
 
 
 @pytest.mark.task0_2
-def test_other() -> None:
+def test_other(a: float) -> None:
     """Write a test that ensures some other property holds for your functions."""
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    """Test the multiplicative inverse property: a * (1/a) = 1"""
+    if abs(a) > 1e-6:
+        assert_close(mul(a, inv(a)), 1.0)
 
 
 # ## Task 0.3  - Higher-order functions
