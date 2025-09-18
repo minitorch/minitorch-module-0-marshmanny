6ab6a25f63ace17175da2a4339716e6c49c2ef7f
AnnaKarpovi <anna.karpoffa@gmail.com>
2025-09-18 13:33:49 +0300

    add operators

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -1,54 +1,108 @@
-"""Collection of the core mathematical operators used throughout the code base."""
+"""
+Collection of the core mathematical operators used throughout the code base.
+"""
 
 import math
-
-# ## Task 0.1
 from typing import Callable, Iterable
 
+# ## Task 0.1
 #
 # Implementation of a prelude of elementary functions.
 
-# Mathematical functions:
-# - mul
-# - id
-# - add
-# - neg
-# - lt
-# - eq
-# - max
-# - is_close
-# - sigmoid
-# - relu
-# - log
-# - exp
-# - log_back
-# - inv
-# - inv_back
-# - relu_back
-#
-# For sigmoid calculate as:
-# $f(x) =  \frac{1.0}{(1.0 + e^{-x})}$ if x >=0 else $\frac{e^x}{(1.0 + e^{x})}$
-# For is_close:
-# $f(x) = |x - y| < 1e-2$
+
+def mul(x: float, y: float) -> float:
+    "$f(x, y) = x * y$"
+    return x * y
+
+def id(x):
+    "$f(x) = x$"
+    return x
+
+def add(x: float, y: float) -> float:
+    "$f(x, y) = x + y$"
+    return x + y
+
+def neg(x: float) -> float:
+    "$f(x) = -x$"
+    return -1.0 * x
+
+def lt(x: float, y: float) -> float:
+    "$f(x) =$ 1.0 if x is less than y else 0.0"
+    return 1.0 if x < y else 0.0
+
+def eq(x: float, y: float) -> float:
+    "$f(x) =$ 1.0 if x is equal to y else 0.0"
+    return 1.0 if x == y else 0.0
+
+def max(x: float, y: float) -> float:
+    "$f(x) =$ x if x is greater than y else y"
+    return x if x >= y else y
 
 
-# TODO: Implement for Task 0.1.
+def is_close(x: float, y: float) -> float:
+    "$f(x) = |x - y| < 1e-2$"
+    return abs(x - y) < 1e-2
+
+
+def sigmoid(x: float) -> float:
+    r"""
+    $f(x) =  \frac{1.0}{(1.0 + e^{-x})}$
+
+    (See https://en.wikipedia.org/wiki/Sigmoid_function )
+
+    Calculate as
+
+    $f(x) =  \frac{1.0}{(1.0 + e^{-x})}$ if x >=0 else $\frac{e^x}{(1.0 + e^{x})}$
+
+    for stability.
+    """
+    if x >= 0:
+        return 1 / (1 + math.exp(-x))
+    else:
+        return math.exp(x) / (1 + math.exp(x))
+
+
+def relu(x: float) -> float:
+    """
+    $f(x) =$ x if x is greater than 0, else 0
+
+    (See https://en.wikipedia.org/wiki/Rectifier_(neural_networks) .)
+    """
+    return x if x >= 0 else 0
+
+
+EPS = 1e-6
+
+def log(x: float) -> float:
+    "$f(x) = log(x)$"
+    return math.log(x + EPS)
+
+
+def exp(x: float) -> float:
+    "$f(x) = e^{x}$"
+    return math.exp(x)
+
+
+def log_back(x: float, d: float) -> float:
+    r"If $f = log$ as above, compute $d \times f'(x)$"
+    return d / (x + EPS)
+
+def inv(x: float) -> float:
+    "$f(x) = 1/x$"
+    return 1 / (x + EPS)
+
+
+def inv_back(x: float, d: float) -> float:
+    r"If $f(x) = 1/x$ compute $d \times f'(x)$"
+    return -d / (x + EPS)**2
+
+
+def relu_back(x: float, d: float) -> float:
+    r"If $f = relu$ compute $d \times f'(x)$"
+    # TODO: Implement for Task 0.1.
+    return d if x > 0 else 0.0
 
 
 # ## Task 0.3
 
 # Small practice library of elementary higher-order functions.
-
-# Implement the following core functions
-# - map
-# - zipWith
-# - reduce
-#
-# Use these to implement
-# - negList : negate a list
-# - addLists : add two lists together
-# - sum: sum lists
-# - prod: take the product of lists
-
-
-# TODO: Implement for Task 0.3.
