f95c6e83cae130138d16dbca78f56fe94d6fd4ad
AnnaKarpovi <anna.karpoffa@gmail.com>
2025-09-18 15:32:33 +0300

    test task03

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -184,8 +184,10 @@ def test_sum_distribute(ls1: List[float], ls2: List[float]) -> None:
     """Write a test that ensures that the sum of `ls1` plus the sum of `ls2`
     is the same as the sum of each element of `ls1` plus each element of `ls2`.
     """
-    # TODO: Implement for Task 0.3.
-    raise NotImplementedError("Need to implement for Task 0.3")
+    sum1 = sum(ls1)
+    sum2 = sum(ls2)
+    sum_of_added_lists = sum(addLists(ls1, ls2))
+    assert_close(sum1 + sum2, sum_of_added_lists)
 
 
 @pytest.mark.task0_3
